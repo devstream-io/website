e7a10bc94213c12e049c3f29006adf5623d97900
ZeekoZhu <vaezt@outlook.com>
2022-06-16 18:44:17 +0800

    chore: how stream works tablet version

--- src/components/how-dev-stream-works.js
+++ src/components/how-dev-stream-works.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
+import Link from '@docusaurus/Link';
 import ArchitectureSvg from '@site/static/img/architecture.svg';
+import ArrowRightSvg from '@site/static/img/arrow-right.svg';
 
 const AdvantageList = [
   {
@@ -23,19 +25,29 @@ export const HowDevStreamWorks = () => {
   return (
     <section className="bg-white py-6 flex flex-col flex-nowrap justify-start items-center">
       <h3 className="section-title">How DevStream Works</h3>
-      <ArchitectureSvg className="mx-4" />
-      <div className="flex flex-col gap-3 mt-5 mx-4">
+      <ArchitectureSvg className="mx-4 sm:mx-6" />
+      <div className="flex flex-col gap-3 mt-5 mx-4 sm:mx-6">
         {AdvantageList.map(({ title, desc }) => (
-          <div key={title} className="flex flex-col justify-start items-center text-center">
+          <div
+            key={title}
+            className="flex flex-col justify-start items-center text-center"
+          >
             <span className="text-heading4 text-neutral-600 font-semibold">
               {title}
             </span>
-            <div className="text-label14 text-neutral-400 mt-1">
-              {desc}
-            </div>
+            <div className="text-label14 text-neutral-400 mt-1">{desc}</div>
           </div>
         ))}
       </div>
+      <Link
+        to="https://docs.devstream.io/en/latest/development/architecture/"
+        className="text-[14px] leading-[1.2] text-primary
+        flex flex-row justify-center items-center gap-1 mt-5
+        hover:text-primary-light
+        "
+      >
+        See Architecture <ArrowRightSvg className="w-[17px] h-[17px] -translate-y-[.12em]" />
+      </Link>
     </section>
   );
 };
